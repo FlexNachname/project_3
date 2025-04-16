845ba1741b6071035688685fe71069ac497763e9
Felix <Fatkidkiter@gmail.com>
2025-04-16 13:24:57 +0200

    pagination funktionsfähig

--- index.js
+++ index.js
@@ -1,8 +1,30 @@
 import { CharacterCard } from "./components/CharacterCard/CharacterCard.js";
 
-async function fetchCharacters() {
-  const response = await fetch("https://rickandmortyapi.com/api/character");
+
+// States
+let maxPage = 1;
+let page = 1;
+const searchQuery = "";
+
+const cardContainer = document.querySelector('[data-js="card-container"]');
+const prevButton = document.querySelector('[data-js="button-prev"]');
+const nextButton = document.querySelector('[data-js="button-next"]');
+
+async function fetchCharacters(currentPage) {
+  const response = await fetch(
+    `https://rickandmortyapi.com/api/character?page=${currentPage}`
+  );
   const data = await response.json();
+  
+  // Maximale Seitenazahl von den API-Daten
+  maxPage = data.info.pages;
+
+  // Aktualisierung der Seiten
+  pagination.textContent = `${page} / ${maxPage} `
+
+  // Buttons disabled für User-exp
+  prevButton.disabled = page == 1;
+  nextButton.disabled = page == maxPage;
 
   // Karte anzeigen (leer)
   cardContainer.innerHTML = "";
@@ -19,22 +41,32 @@ async function fetchCharacters() {
   }
 }
 
-fetchCharacters();
+prevButton.addEventListener("click", () => {
+  if (page > 1) {
+    page--;
+    fetchCharacters(page);
+  }
+});
 
-const cardContainer = document.querySelector('[data-js="card-container"]');
+nextButton.addEventListener("click", () => {
+  if (page < maxPage) {
+    page++;
+    fetchCharacters(page);
+  }
+});
+
+
+
+fetchCharacters(page);
+
+
 const searchBarContainer = document.querySelector(
   '[data-js="search-bar-container"]'
 );
 const searchBar = document.querySelector('[data-js="search-bar"]');
 const navigation = document.querySelector('[data-js="navigation"]');
-const prevButton = document.querySelector('[data-js="button-prev"]');
-const nextButton = document.querySelector('[data-js="button-next"]');
 const pagination = document.querySelector('[data-js="pagination"]');
 
-// States
-const maxPage = 1;
-const page = 1;
-const searchQuery = "";
 
-//cardContainer.innerHTML = "";
+
 // cardContainer.append(CharacterCard());
