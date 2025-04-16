e8e1d7c10cff813050a804441f8eabf289050d44
Felix <Fatkidkiter@gmail.com>
2025-04-16 15:15:39 +0200

    search bar angelegt und funktionalität hergestellt

--- index.js
+++ index.js
@@ -1,26 +1,32 @@
 import { CharacterCard } from "./components/CharacterCard/CharacterCard.js";
 
-
 // States
 let maxPage = 1;
 let page = 1;
-const searchQuery = "";
+let query = "";
+
+const searchBarContainer = document.querySelector(
+  '[data-js="search-bar-container"]'
+);
+const searchBar = document.querySelector('[data-js="search-bar"]');
+const navigation = document.querySelector('[data-js="navigation"]');
+const pagination = document.querySelector('[data-js="pagination"]');
 
 const cardContainer = document.querySelector('[data-js="card-container"]');
 const prevButton = document.querySelector('[data-js="button-prev"]');
 const nextButton = document.querySelector('[data-js="button-next"]');
 
-async function fetchCharacters(currentPage) {
+async function fetchCharacters(currentPage, searchQuery = "") {
   const response = await fetch(
-    `https://rickandmortyapi.com/api/character?page=${currentPage}`
+    `https://rickandmortyapi.com/api/character?page=${currentPage}&name=${searchQuery}`
   );
   const data = await response.json();
-  
+
   // Maximale Seitenazahl von den API-Daten
   maxPage = data.info.pages;
 
   // Aktualisierung der Seiten
-  pagination.textContent = `${page} / ${maxPage} `
+  pagination.textContent = `${page} / ${maxPage} `;
 
   // Buttons disabled für User-exp
   prevButton.disabled = page == 1;
@@ -44,29 +50,42 @@ async function fetchCharacters(currentPage) {
 prevButton.addEventListener("click", () => {
   if (page > 1) {
     page--;
-    fetchCharacters(page);
+    fetchCharacters(page, query);
+
+    // Scrollt zum oberen Rand der Seite
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
   }
 });
 
 nextButton.addEventListener("click", () => {
   if (page < maxPage) {
     page++;
-    fetchCharacters(page);
+    fetchCharacters(page, query);
+
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
   }
 });
 
+fetchCharacters(page, "");
 
+searchBar.addEventListener("submit", (event) => {
+  // verhindert Reload
+  event.preventDefault();
 
-fetchCharacters(page);
+  const formData = new FormData(event.target);
+  const searchText = Object.fromEntries(formData);
+  query = searchText.query;
+  // neu laden mit Suchwort
+  fetchCharacters(page, query);
 
+  searchBar.reset();
+});
 
-const searchBarContainer = document.querySelector(
-  '[data-js="search-bar-container"]'
-);
-const searchBar = document.querySelector('[data-js="search-bar"]');
-const navigation = document.querySelector('[data-js="navigation"]');
-const pagination = document.querySelector('[data-js="pagination"]');
-
-
-
-// cardContainer.append(CharacterCard());
+// Nicht benötigt?
+//const query = formData.get("query").trim();
